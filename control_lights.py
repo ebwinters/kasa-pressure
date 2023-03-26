4b87faf2ef17f79ca1c266ec08b470a3c7059064
ebwinters@comcast.net <ebwinters@comcast.net>
2023-03-26 09:55:10 -0700

    script is working

--- control_lights.py
+++ control_lights.py
@@ -5,26 +5,28 @@ def getBulbs(deviceDict):
     ips = [ip for ip in deviceDict]
     return list(filter(lambda ip: deviceDict[ip].device_type == DeviceType.Bulb, ips))
 
-async def up_down(ip):
-    bulb = SmartBulb(ip)
-    await bulb.update()
-    if (bulb.is_on):
-        await bulb.turn_off(transition=2_000)
+async def up_down(ips):
+    shouldTurnOn = False
+    for index, ip in enumerate(ips):
+        bulb = SmartBulb(ip)
         await bulb.update()
-    else:
-        await bulb.turn_on()
-        await bulb.update()
-        await bulb.set_brightness(100)
-        await bulb.set_color_temp(2500)
-        await bulb.update()
-        
+        if (index == 0):
+            shouldTurnOn = bulb.is_off
+        if (shouldTurnOn):
+            await bulb.set_brightness(100)
+            await bulb.set_color_temp(2500)
+            await bulb.update()
+            await bulb.turn_on(transition=2_000)
+        else:
+            await bulb.turn_off(transition=2_000)
+            
 
 async def main():
     found_devices = await Discover.discover()
-    print (found_devices)
+    print (len(found_devices))
     bulb_ips = getBulbs(found_devices)
-    for bulb_ip in bulb_ips:
-        await up_down(bulb_ip)
+    await up_down(bulb_ips)
+    
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
