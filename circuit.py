b4905c2e738a57e8606d773b4b882c3808526ef3
ebwinters <ebwinters@comcast.net>
2023-04-19 11:14:26 -0700

    priv working

--- circuit.py
+++ circuit.py
@@ -1,25 +1,34 @@
 import RPi.GPIO as GPIO
 import asyncio
 from control_lights import *
-import sys
 import redis
+import getpass
+import sys
 
+print ('a')
+
+print(getpass.getuser())
 loop = None
-r = redis.Redis(host='localhost', port=6379)
-
+print ('b')
+r = redis.Redis(host='localhost', port=6379, decode_responses=True)
+print ('c')
 def button_callback(up_down):
     print('changing voltage event detected')
-    bulb_ips = r.get('bulb_ips').split(',')
+    bulb_ips = str(r.get('bulb_ips')).split(',')
     asyncio.run(up_down(bulb_ips))
     
 
 if __name__ == '__main__':
     try:
+        print ('d')
         # setup the GPIO
         GPIO.setwarnings(False) # Ignore warning for now
+        print ('e')
         GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+        print ('f')
         GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
+        print ('g')
         GPIO.add_event_detect(10, GPIO.RISING, callback=lambda x: button_callback(up_down), bouncetime=500)
         print ("Running smart light circuit...")
         # run the event loop
