f860183dae1d568e6dce89ad623ccec3e662485d
ebwinters <ebwinters@comcast.net>
2023-04-20 19:26:29 -0700

    check len

--- circuit.py
+++ circuit.py
@@ -2,16 +2,10 @@ import RPi.GPIO as GPIO
 import asyncio
 from control_lights import *
 import redis
-import getpass
 import sys
 
-print ('a')
-
-print(getpass.getuser())
 loop = None
-print ('b')
 r = redis.Redis(host='localhost', port=6379, decode_responses=True)
-print ('c')
 def button_callback(up_down):
     print('changing voltage event detected')
     bulb_ips = str(r.get('bulb_ips')).split(',')
@@ -20,15 +14,10 @@ def button_callback(up_down):
 
 if __name__ == '__main__':
     try:
-        print ('d')
         # setup the GPIO
         GPIO.setwarnings(False) # Ignore warning for now
-        print ('e')
         GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-        print ('f')
         GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
-
-        print ('g')
         GPIO.add_event_detect(10, GPIO.RISING, callback=lambda x: button_callback(up_down), bouncetime=500)
         print ("Running smart light circuit...")
         # run the event loop
--- update_ips.py
+++ update_ips.py
@@ -6,9 +6,11 @@ async def main():
     r = redis.Redis(host='localhost', port=6379)
     while True:
         bulb_ips = await discover_bulbs()
+        if len(bulb_ips) != 2:
+            return
         print ("Found bulb IPS: ", str(bulb_ips))
         r.set('bulb_ips', ','.join(bulb_ips))
-        await asyncio.sleep(10)
+        await asyncio.sleep(3600)
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
