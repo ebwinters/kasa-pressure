e66392ac8315f0d3f8bc90b5a582cc22a7216151
ebwinters <ebwinters@comcast.net>
2023-04-23 09:21:54 -0700

    bounce works

--- circuit.py
+++ circuit.py
@@ -3,15 +3,21 @@ import asyncio
 from control_lights import *
 import redis
 import sys
+import time
 
 loop = None
 r = redis.Redis(host='localhost', port=6379, decode_responses=True)
 def button_callback(up_down):
+    time.sleep(0.25)
+    GPIO.remove_event_detect(10)
     print('changing voltage event detected')
     bulb_ips = str(r.get('bulb_ips')).split(',')
     asyncio.run(up_down(bulb_ips))
-    sleep(1)
+    time.sleep(0.25)
+    setup_gpio_event()
     
+def setup_gpio_event():
+    GPIO.add_event_detect(10, GPIO.RISING, callback=lambda x: button_callback(up_down), bouncetime=500)
 
 if __name__ == '__main__':
     try:
@@ -19,7 +25,7 @@ if __name__ == '__main__':
         GPIO.setwarnings(False) # Ignore warning for now
         GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
         GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
-        GPIO.add_event_detect(10, GPIO.RISING, callback=lambda x: button_callback(up_down), bouncetime=500)
+        setup_gpio_event()
         print ("Running smart light circuit...")
         # run the event loop
         loop = asyncio.get_event_loop()
