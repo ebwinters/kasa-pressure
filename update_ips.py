1429141712a75ba26f283fd3f312eddb95594371
ebwinters@comcast.net <ebwinters@comcast.net>
2023-05-25 07:51:16 -0700

    update ip retries

--- update_ips.py
+++ update_ips.py
@@ -2,15 +2,19 @@ import redis
 import asyncio
 from discover import *
 
+expected_bulbs = 3
+
 async def main():
     r = redis.Redis(host='localhost', port=6379)
     while True:
-        bulb_ips = await discover_bulbs()
-        if len(bulb_ips) == 3:
-            print ("Found bulb IPS: ", str(bulb_ips))
-            r.set('bulb_ips', ','.join(bulb_ips))
-        else:
-            print ("Incorrect amount of bulb IPS found: ", str(len(bulb_ips))) 
+        for retry in range(10):
+            bulb_ips = await discover_bulbs()
+            if len(bulb_ips) == 3:
+                print ("Found bulb IPS: ", str(bulb_ips), " on retry ", retry)
+                r.set('bulb_ips', ','.join(bulb_ips))
+                break
+            else:
+                print ("Incorrect amount of bulb IPS found: ", str(len(bulb_ips)), " retry ", retry) 
         await asyncio.sleep(3600)
 
 if __name__ == "__main__":
